461a5fb99e1065b47ee7000ad3eb3ca67c0f5415
Guillaume J Humbert <guillaume.j.humbert@pm.me>
2025-04-29 17:15:31 +0800

    upgrade plugins, preview horizontal scroll in telescope

--- lua/guillaume/plugins/telescope.lua
+++ lua/guillaume/plugins/telescope.lua
@@ -1,5 +1,15 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
+--  tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    defaults = {
+      mappings = {
+        i = {
+          ["<C-Left>"] = "preview_scrolling_left",
+          ["<C-Right>"] = "preview_scrolling_right",
+        },
+      },
+    },
+  },
 }

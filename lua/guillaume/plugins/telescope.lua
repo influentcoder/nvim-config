3f91bddc8f683b0b7bcae20bd806c590f0f0ef62
Guillaume J Humbert <guillaume.j.humbert@pm.me>
2025-04-29 17:21:12 +0800

    fix telescope keymaps

--- lua/guillaume/plugins/telescope.lua
+++ lua/guillaume/plugins/telescope.lua
@@ -1,13 +1,12 @@
 return {
   'nvim-telescope/telescope.nvim',
---  tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = {
     defaults = {
       mappings = {
         i = {
-          ["<C-Left>"] = "preview_scrolling_left",
-          ["<C-Right>"] = "preview_scrolling_right",
+          ["<M-Left>"] = "preview_scrolling_left",
+          ["<M-Right>"] = "preview_scrolling_right",
         },
       },
     },

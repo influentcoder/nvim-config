b1222ed13593cc7d3948181b30394fc37f9f8dcd
Guillaume J Humbert <guillaume.j.humbert@pm.me>
2025-05-22 06:10:16 +0800

    add lua_ls, better telescope search opts, upgrade plugins

--- lua/guillaume/plugins/telescope.lua
+++ lua/guillaume/plugins/telescope.lua
@@ -7,10 +7,46 @@ return {
         i = {
           ["<M-Left>"] = "preview_scrolling_left",
           ["<M-Right>"] = "preview_scrolling_right",
-          ["<M-D>" ] = "delete_buffer",
+          ["<M-D>"] = "delete_buffer",
         },
         n = {
-          ["<M-D>" ] = "delete_buffer",
+          ["<M-D>"] = "delete_buffer",
+        },
+      },
+      vimgrep_arguments = {
+        'rg',
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+        '--smart-case',
+        '--hidden',
+        "--glob=!**/.git/*",
+        "--glob=!**/.idea/*",
+        "--glob=!**/.vscode/*",
+        "--glob=!**/build/*",
+        "--glob=!**/dist/*",
+        "--glob=!**/yarn.lock",
+        "--glob=!**/package-lock.json",
+      },
+    },
+    pickers = {
+      find_files = {
+        hidden = true,
+        -- needed to exclude some files & dirs from general search
+        -- when not included or specified in .gitignore
+        find_command = {
+          "rg",
+          "--files",
+          "--hidden",
+          "--glob=!**/.git/*",
+          "--glob=!**/.idea/*",
+          "--glob=!**/.vscode/*",
+          "--glob=!**/build/*",
+          "--glob=!**/dist/*",
+          "--glob=!**/yarn.lock",
+          "--glob=!**/package-lock.json",
         },
       },
     },
